c7e8a121f0a9834e3e58181b2454ceed51a28efb
Kervcodes-desktop <Kervintz84@hotmail.com>
2020-03-09 22:58:24 -0400

    set route paths

--- app.js
+++ app.js
@@ -1,4 +1,33 @@
 //Import Express
 const express = require("express");
 //
-const app = app.express();
+const app = express();
+
+
+//set view engine setup
+app.set('view engine', 'pug');
+
+//Static middleware
+app.use(express.static('public'))
+
+
+/**
+ * Set routes
+ */
+
+//index
+app.get('/', (req, res) => {
+  res.render('index')
+});
+
+app.get('/about', (req, res) => {
+  res.render('about')
+});
+
+app.get('/projects', (req, res) => {
+  res.render('project')
+});
+
+app.listen(3001, () => {
+  console.log('SERVER LISTENING ON PORT 3000')
+})
