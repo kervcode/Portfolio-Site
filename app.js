5b350ab5b79650f951e3dbc1db28b4569f759527
Kervintz Noel <10268341+kervcode@users.noreply.github.com>
2020-04-29 17:08:11 -0400

    start to handle errors

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 //Import Express
 const express = require("express");
+// Import body-parser middle
+const bodyParser = require('body-parser');
 //require the data file
 const data = require("./data/data.json").projects;
 
@@ -11,13 +13,21 @@ app.set("view engine", "pug");
 
 //Static middleware
 // app.use(express.static('public'));
+app.use(bodyParser.json());
 app.use("/static", express.static("public"));
 // app.use(express.static(__dirname + '/public'));
 
 // Middleware
 app.use((req, res, next) => {
-  // console.log(data[0]);
+  console.log(req.params);
   next();
+});
+
+// Error Middleware
+app.use((err, req, res, next) => {
+  res.locals.error = err
+  res.status(500)
+  res.render('error')
 });
 /**
  * Set routes
@@ -39,13 +49,13 @@ app.get("/projects", (req, res) => {
 app.get("/projects/:id", (req, res) => {
   const id = parseInt(req.params.id);
   // console.log(id);
-  res.render("project", {
-    title: data[id].project_name,
-    description: data[id].description,
-    image: data[id].image_urls,
-    technologies: data[id].technologies,
-    live_link: data[id].live_link,
-    github_link: data[id].github_link,
+  res.render("project", { data
+    // title: data[id].project_name,
+    // description: data[id].description,
+    // image: data[id].image_urls,
+    // technologies: data[id].technologies,
+    // live_link: data[id].live_link,
+    // github_link: data[id].github_link,
   });
 });
 
